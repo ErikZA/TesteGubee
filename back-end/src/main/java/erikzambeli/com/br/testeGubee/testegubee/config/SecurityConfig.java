5fdbb1407812c1c40683e757005e73afce438c0f
Erik Henrique De Oliveira  Zambeli <erik_12_nf@live.com>
2019-12-12 01:19:38 -0300

    Update SecurityConfig.java

--- back-end/src/main/java/erikzambeli/com/br/testeGubee/testegubee/config/SecurityConfig.java
+++ back-end/src/main/java/erikzambeli/com/br/testeGubee/testegubee/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest()
+        http.csrf().disable().authorizeRequests().anyRequest()
                 .authenticated().and().httpBasic();
     }
 
